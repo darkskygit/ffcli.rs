9e29bf80099d7d10731cb0a97a55356b03968e96
DarkSky <darksky2048@gmail.com>
2019-05-31 21:22:29 +0800

    cleanup

--- mod.rs
+++ mod.rs
@@ -5,18 +5,3 @@ mod preset;
 pub use args::{FFmpegArgs, FFmpegDefaultArgs};
 pub use filter::{VideoFilter, VideoFilterParams};
 pub use preset::VideoPreset;
-
-pub fn info(default: Option<FFmpegDefaultArgs>) -> Vec<String> {
-    let filter = VideoFilter::new().input(0).output(1).params("test", "");
-    let filter2 = VideoFilter::new()
-        .params("alpha", "qwlidouoasd:asdsd")
-        .params("sddd".to_string(), "None");
-    FFmpegArgs::new()
-        .vf(filter)
-        // .vf(filter2)
-        .build_filter()
-        // .vf(filter)
-        .vf(filter2)
-        .build_filter()
-        .build(default)
-}
